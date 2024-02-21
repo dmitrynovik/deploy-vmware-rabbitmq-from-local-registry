36ed3edeb5992832fc654044342588f02669e3ee
dmitrynovik <dmitry.novik@gmail.com>
2024-02-21 17:19:25 +1100

    feat: schema replication works

---
+++ rabbitmq.crd-examples/warm-standby-replication/get-generated-users.sh
@@ -0,0 +1,18 @@
+ns_upstream="rabbitmq-system"
+cluster_upstream="rabbitmq-1-upstream"
+
+ns_downstream="rabbitmq-system"
+cluster_downstream="rabbitmq-1-downstream"
+
+
+echo -e "Upstream user:"
+sudo kubectl -n $ns_upstream get secret "$cluster_upstream-default-user" -o jsonpath='{.data.username}' | base64 -d
+echo -e "\nUpstream password:"
+sudo kubectl -n $ns_upstream get secret "$cluster_upstream-default-user" -o jsonpath='{.data.password}' | base64 -d
+
+echo -e "\n\nDownstream user:"
+sudo kubectl -n $ns_downstream get secret "$cluster_downstream-default-user" -o jsonpath='{.data.username}' | base64 -d
+echo -e "\nDownstream password:"
+sudo kubectl -n $ns_downstream get secret "$cluster_downstream-default-user" -o jsonpath='{.data.password}' | base64 -d
+
+echo -e "\n"
